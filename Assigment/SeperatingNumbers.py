591dddbf34624a656f06c1aed4f049a406b43f1d
OmodaraAyo <ayodeleomodara1234@gmail.com>
2024-07-13 12:26:14 +0100

    first commit

--- Assigment/SeperatingNumbers.py
+++ Assigment/SeperatingNumbers.py
@@ -1,7 +1,8 @@
-number = int (input ('Enter a number:'))
-number2 = int (input ('Enter a number:'))
-number3 = int (input ('Enter a number:'))
-number4 = int (input ('Enter a number:'))
-number5 = int (input ('Enter a number:'))
+number = int (input ('Enter five digit number: '))
+number1 = number // 10000
+number2 = (number // 1000 ) % 10
+number3 = (number // 100) % 10
+number4 = (number // 10) % 10
+number5 = number % 10
 
-print (number, number2, number3, number4, number5)
+print (number1, number2, number3, number4, number5)
